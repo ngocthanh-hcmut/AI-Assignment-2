c64f1f2bf9553a0acaaf0c2eb625afc83264524d
Leo <thinhphatmai2001@gmail.com>
2022-11-30 19:46:52 +0700

    new

---
+++ Phat/main.py
@@ -0,0 +1,2 @@
+def move(prev_board, board, player, remain_time_x, remain_time_o):
+    pass
---
+++ Phat/source.py
@@ -0,0 +1,6 @@
+def minimax(position, depth, who):
+    if depth = 0:
+        value = evalStatic(position)
+    return value
+
+    
