4b13dcf77883b2d458b725779c8e30351abcabd5
ngocthanh-hcmut <thanh.ho.it@hcmut.edu.vn>
2022-12-27 20:24:33 +0700

    .

--- Thanh/main.py
+++ Thanh/main.py
@@ -1,5 +1,20 @@
 from algorithm import *
 import copy
+import random
+
+def random_move(prev_board, board, player):
+    pieces = []
+
+    for x in range(5):
+        for y in range(5):
+            if board[x][y] == player:
+                pieces.append((x,y))
+
+    valid_moves = get_valid_moves(prev_board, board, player, pieces)
+
+    num = random.randint(0, len(valid_moves) - 1)
+
+    return valid_moves[num]
 
 
 def end_game(board):
@@ -41,27 +56,39 @@ def play():
     printBoard(board)
 
     temp_pieces = set()
-    while not end_game(board):
-        print("Your move:")
-        input_move = list(map(int, input().split()))
-        src_x = input_move[0]
-        src_y = input_move[1]
-        dest_x = input_move[2]
-        dest_y = input_move[3]
+    step = 0
+    while True:
+        print(step)
 
-        human_move = ((src_x, src_y), (dest_x, dest_y))
+        # print("Your move:")
+        # input_move = list(map(int, input().split()))
+        # src_x = input_move[0]
+        # src_y = input_move[1]
+        # dest_x = input_move[2]
+        # dest_y = input_move[3]
+        # human_move = ((src_x, src_y), (dest_x, dest_y))
+
+        human_move = random_move(prev_board, board, -1)
+
+        # human_move = move(prev_board, board, -1, 999, 999, 2)
+
 
         prev_board = copy.deepcopy(board)
         act_move(board, human_move, -1, temp_pieces)
 
         print("After your move:")
         printBoard(board)
+        if end_game(board): return
 
-        machine_move = move(prev_board, board, 1, 999, 999)
+        machine_move = move(prev_board, board, 1, 999, 999, 3)
 
         act_move(board, machine_move, 1, temp_pieces)
         print("After machine move:")
         printBoard(board)
+        if end_game(board): return
+
+        step += 1
+
 
 
 play()
--- Thanh/test.py
+++ Thanh/test.py
@@ -16,20 +16,35 @@ def printBoard(board):
         print('\t'.join(map(str, i)))
         print()
 
-temp_pieces = set()
+# temp_pieces = set()
 
-prev_board = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, -1, 1], [1, 1, 1, 1, 0]]
+# prev_board = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, -1, 1], [1, 1, 1, 1, 0]]
 
-board = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, -1]]
+# board = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, -1]]
 
 
-printBoard(prev_board)
-print("------------------------------------------")
+# printBoard(prev_board)
+# print("------------------------------------------")
+# printBoard(board)
+# print("------------------------------------------")
+
+# machine_move = move(prev_board, board, 1, 999, 999)
+
+# # machine_move = ((2,4), (3,3))
+# act_move(board, machine_move, 1, temp_pieces)
+# printBoard(board)
+
+
+prev_board = None
+
+board = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, -1], [-1, 0, 0, 0, -1], [-1, -1, -1, -1, -1]]
+
 printBoard(board)
-print("------------------------------------------")
 
-machine_move = move(prev_board, board, 1, 999, 999)
+pieces = []
+for x in range(5):
+    for y in range(5):
+        if board[x][y] == 1:
+            pieces.append((x,y))
 
-# machine_move = ((2,4), (3,3))
-act_move(board, machine_move, 1, temp_pieces)
-printBoard(board)
+print(get_valid_moves(prev_board, board, 1, pieces))
